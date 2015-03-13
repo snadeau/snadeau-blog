eac6964cb96156fe2699c11fc3cdf07c7c961111
Scott Nadeau <snadeau1@gmail.com>
2015-03-13 12:51:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-SECRET = 'vo2Hc4Ebs3rA9'
+SECRET = 'asdf'
 
 #
 #Entities
